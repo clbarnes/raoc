811efea9bb6c7379a2eb948131c4265147fbf43d
Chris Barnes <barnesc@janelia.hhmi.org>
2020-12-14 11:10:54 +0000

    build files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="raoc",
+    version="0.1.0",
+    url="https://github.com/clbarnes/raoc",
+    author="Chris L. Barnes",
+    author_email="chrislloydbarnes@gmail.com",
+    description=(
+        "Script to automate random acts of coffee: "
+        "random scheduled meetups within a group"
+    ),
+    py_modules=["raoc"],
+    install_requires=["yagmail", "strictyaml"],
+)
